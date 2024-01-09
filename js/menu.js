beff417e30b7e3254f19506f7f31e9e42036d2ed
DiegoCanales <diegocanales112@gmail.com>
2024-01-09 02:12:00 -0300

    Se ingreso formulario de correo mobile

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,25 @@
 const menu = document.querySelector(".menu");
 const abrirMenu = document.querySelector(".abrir-menu");
 const cerrarMenu = document.querySelector(".cerrar-menu");
+const enlacesMenu = document.querySelectorAll(".menuTexto");
+const contenedorBotonEnviar = document.querySelector(".contenedor-boton-enviar");
 
-function usarMenu(){
+function usarMenu() {
     menu.classList.toggle("menu-abierto");
+    
+    // Cambia la lógica para agregar/quitar la clase ocultar-botones
+    if (menu.classList.contains("menu-abierto")) {
+        contenedorBotonEnviar.classList.add("ocultar-botones");
+    } else {
+        contenedorBotonEnviar.classList.remove("ocultar-botones");
+    }
 }
 
 abrirMenu.addEventListener("click", usarMenu);
 cerrarMenu.addEventListener("click", usarMenu);
+
+enlacesMenu.forEach((enlace) => {
+    enlace.addEventListener("click", () => {
+        usarMenu();
+    });
+});
